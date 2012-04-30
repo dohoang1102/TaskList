76dc06cafa59e973d01c4151b0a4d7adb9d5cff1
Clement Rousselle <clement.rousselle@hotappsfactory.com>
2012-04-30 10:29:19 +0200

    Initial Commit

---
+++ LogSafeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LogSafeTest
+//
+//  Created by Clément Rousselle on 4/30/12.
+//  Copyright (c) 2012 Hot Apps Factroy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LogSafeTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LogSafeTest
+//
+//  Created by Clément Rousselle on 4/30/12.
+//  Copyright (c) 2012 Hot Apps Factroy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
